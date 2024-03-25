a607074b5d6f89a6ce5872b152144ac82604163c
TahiaBushra <bushratahia@gmail.com>
2024-03-26 01:08:49 +0600

    create user req

--- src/providers/MyAuth0Provider.tsx
+++ src/providers/MyAuth0Provider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Auth0Provider } from "@auth0/auth0-react";
+import { useCreateUser } from "@/hooks/useCreateUser";
+import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 import { PropsWithChildren } from "react";
 
 const MyAuth0Provider = ({ children }: PropsWithChildren) => {
@@ -8,11 +9,17 @@ const MyAuth0Provider = ({ children }: PropsWithChildren) => {
   const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID;
   const callbackUri = process.env.NEXT_PUBLIC_AUTH0_CALLBACK_URI;
 
+  const { createUser } = useCreateUser();
+
   if (!domain || !clientId || !callbackUri) {
     throw new Error("Unable to initialize auth0");
   }
 
-  const onRedirectCallback = () => {};
+  const onRedirectCallback = (appState?: AppState, user?: User) => {
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
+  };
 
   return (
     <Auth0Provider
