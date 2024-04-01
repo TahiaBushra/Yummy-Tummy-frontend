f28954010ef144a7ea58047d94a1bbd997a39d75
TahiaBushra <bushratahia@gmail.com>
2024-04-02 00:26:59 +0600

    get user req

--- src/app/user-profile/page.tsx
+++ src/app/user-profile/page.tsx
@@ -1,13 +1,30 @@
 "use client";
 
+import Error from "@/components/Error";
+import Loading from "@/components/Loading";
 import UserProfileForm from "@/components/UserProfileForm";
+import { useGetUser } from "@/hooks/useGetUser";
 import { useUpdateUser } from "@/hooks/useUpdateUser";
 
 const UserProfilePage = () => {
+  const { currentUser, isLoading: isGetLoading } = useGetUser();
   const { isLoading: isUpdateLoading, updateUser } = useUpdateUser();
+
+  if (isGetLoading) {
+    return <Loading />;
+  }
+
+  if (!currentUser) {
+    return <Error message="Unable to load user profile" />;
+  }
+
   return (
     <div>
-      <UserProfileForm isLoading={isUpdateLoading} onSave={updateUser} />
+      <UserProfileForm
+        currentUser={currentUser}
+        isLoading={isUpdateLoading}
+        onSave={updateUser}
+      />
     </div>
   );
 };
