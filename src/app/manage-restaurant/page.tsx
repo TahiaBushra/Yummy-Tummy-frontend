1b80bce6528d45d2d3daab07db732690ba0cac6d
TahiaBushra <bushratahia@gmail.com>
2024-05-09 00:36:37 +0600

    submit done

--- src/app/manage-restaurant/page.tsx
+++ src/app/manage-restaurant/page.tsx
@@ -1,7 +1,14 @@
-import ManageRestaurantForm from "@/components/ManageRestaurantForm.1";
+"use client";
+
+import ManageRestaurantForm from "@/components/ManageRestaurantForm";
+import { useCreateRestaurant } from "@/hooks/useCreateRestaurant";
 
 const ManageRestaurantPage = () => {
-  return <ManageRestaurantForm />;
+  const { createRestaurant, isLoading } = useCreateRestaurant();
+
+  return (
+    <ManageRestaurantForm isLoading={isLoading} onSave={createRestaurant} />
+  );
 };
 
 export default ManageRestaurantPage;
