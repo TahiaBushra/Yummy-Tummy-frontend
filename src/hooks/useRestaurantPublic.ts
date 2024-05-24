85d487e5a3364d5607c0dc0b77b2d8670a5d8898
TahiaBushra <bushratahia@gmail.com>
2024-05-25 00:48:40 +0600

    search page api req

---
+++ src/hooks/useRestaurantPublic.ts
@@ -0,0 +1,27 @@
+import { API_BASE_URL } from "@/constants";
+import { RestaurantSearchResponse } from "@/types";
+import { useQuery } from "react-query";
+
+export const useRestaurantPublic = (city?: string) => {
+  const createSearchRequest = async (): Promise<RestaurantSearchResponse> => {
+    const params = new URLSearchParams();
+
+    const res = await fetch(
+      `${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to get restaurants");
+    }
+
+    return res.json();
+  };
+
+  const { data: results, isLoading } = useQuery(
+    ["SearchRestaurants"],
+    createSearchRequest,
+    { enabled: !!city }
+  );
+
+  return { results, isLoading };
+};
